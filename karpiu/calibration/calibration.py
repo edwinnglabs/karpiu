cd0c3398600ed002593ebe864fe2133f7ca3d90b
edwinnglabs <edwinnglabs@gmail.com>
2023-12-17 22:18:27 -0800

    formmating

--- karpiu/calibration/calibration.py
+++ karpiu/calibration/calibration.py
@@ -19,7 +19,7 @@ class CalibrationProcess:
         model: MMM,
         calib_tests_df: pd.DataFrame,
         logger: Optional[logging.Logger] = None,
-        se_tolerance: float = 1.,
+        se_tolerance: float = 1.0,
         silent: bool = False,
     ):
         if logger is None:
@@ -29,7 +29,7 @@ class CalibrationProcess:
                 self.logger.setLevel(30)
         else:
             self.logger = logger
-        
+
         self.base_mod = model.copy()
         self.curr_mod = model.copy()
 
@@ -37,7 +37,9 @@ class CalibrationProcess:
         self.calibrated_channels = list()
         self.se_tolerance = se_tolerance
         self.init_tests_df = calib_tests_df.copy()
-        init_report = CalibrationProcess.report_attribution(self.curr_mod, self.init_tests_df)
+        init_report = CalibrationProcess.report_attribution(
+            self.curr_mod, self.init_tests_df
+        )
         init_report = CalibrationProcess.append_model_info(self.curr_mod, init_report)
         init_report["iter"] = 0
 
@@ -67,14 +69,17 @@ class CalibrationProcess:
         # sorted in descending order
         calibrating_channels = self.select_calib_channels(self.calib_reports[-1])
         self.logger.info(
-            "{}/{} channels required to be calibrated:\n {}"
-            .format(
-            len(calibrating_channels), 
-            self.calib_reports[0].shape[0],
-            calibrating_channels,
+            "{}/{} channels required to be calibrated:\n {}".format(
+                len(calibrating_channels),
+                self.calib_reports[0].shape[0],
+                calibrating_channels,
             )
         )
-        self.logger.info("Total target distance: {}".format(self.calib_reports[-1]["target_dist"].sum()))
+        self.logger.info(
+            "Total target distance: {}".format(
+                self.calib_reports[-1]["target_dist"].sum()
+            )
+        )
 
         while (iter < max_iters) and len(calibrating_channels) > 0:
             self.logger.info(f"Iteration: {iter}/{max_iters}")
@@ -82,10 +87,12 @@ class CalibrationProcess:
             # pick the largest distance channel
             target_channel = calibrating_channels[0]
             self.logger.info(f"Calibrating channel: {target_channel}")
-            loc_prior = self._solve_channel_prior(curr_mod, target_channel, self.calib_reports[-1])
+            loc_prior = self._solve_channel_prior(
+                curr_mod, target_channel, self.calib_reports[-1]
+            )
 
             new_priors_df = CalibrationProcess.generate_prior(
-                model = curr_mod,
+                model=curr_mod,
                 modify_channel=target_channel,
                 loc_prior=loc_prior,
             )
@@ -117,7 +124,9 @@ class CalibrationProcess:
             curr_mod = new_mod
             iter += 1
 
-            new_report = CalibrationProcess.report_attribution(curr_mod, self.init_tests_df)
+            new_report = CalibrationProcess.report_attribution(
+                curr_mod, self.init_tests_df
+            )
             new_report = CalibrationProcess.append_model_info(curr_mod, new_report)
             new_report["iter"] = iter
 
@@ -126,14 +135,17 @@ class CalibrationProcess:
 
             calibrating_channels = self.select_calib_channels(new_report)
             self.logger.info(
-                "{}/{} channels required to be calibrated:\n {}"
-                .format(
-                len(calibrating_channels), 
-                self.calib_reports[0].shape[0],
-                calibrating_channels,
+                "{}/{} channels required to be calibrated:\n {}".format(
+                    len(calibrating_channels),
+                    self.calib_reports[0].shape[0],
+                    calibrating_channels,
                 )
             )
-            self.logger.info("Total target distance: {}".format(self.calib_reports[-1]["target_dist"].sum()))
+            self.logger.info(
+                "Total target distance: {}".format(
+                    self.calib_reports[-1]["target_dist"].sum()
+                )
+            )
 
         self.curr_mod = curr_mod
 
@@ -141,7 +153,11 @@ class CalibrationProcess:
         return self.curr_mod.copy()
 
     @staticmethod
-    def plot_cost_calib(calib_report_df: pd.DataFrame, calibrated_steps: pd.DataFrame, is_visible: bool = True):
+    def plot_cost_calib(
+        calib_report_df: pd.DataFrame,
+        calibrated_steps: pd.DataFrame,
+        is_visible: bool = True,
+    ):
         regressors = calib_report_df["regressor"].unique().tolist()
         nrows = math.ceil(len(regressors) / 2)
 
@@ -158,13 +174,24 @@ class CalibrationProcess:
 
             axes[idx].set_title(ch)
             axes[idx].plot(x, y_mmm, label="mmm", color="orange", alpha=0.5)
-            axes[idx].plot(x, y_target, linestyle="--", label="target", color="dodgerblue", alpha=0.5)
-            axes[idx].fill_between(x, y_target_lower, y_target_upper, alpha=0.2, color="dodgerblue")
+            axes[idx].plot(
+                x,
+                y_target,
+                linestyle="--",
+                label="target",
+                color="dodgerblue",
+                alpha=0.5,
+            )
+            axes[idx].fill_between(
+                x, y_target_lower, y_target_upper, alpha=0.2, color="dodgerblue"
+            )
 
-            mask = calibrated_steps['regressor'] == ch
+            mask = calibrated_steps["regressor"] == ch
             if np.any(mask):
-                x_steps = calibrated_steps.loc[mask, 'iter']                
-                axes[idx].scatter(x_steps, y_mmm[x_steps], color="orange", label="calibration")
+                x_steps = calibrated_steps.loc[mask, "iter"]
+                axes[idx].scatter(
+                    x_steps, y_mmm[x_steps], color="orange", label="calibration"
+                )
 
         fig.suptitle(
             "Cost Calibration", y=0.92, fontsize=28, verticalalignment="center"
@@ -190,7 +217,11 @@ class CalibrationProcess:
         return axes
 
     @staticmethod
-    def plot_coef_calib(calib_report_df: pd.DataFrame, calibrated_steps: pd.DataFrame, is_visible: bool = True):
+    def plot_coef_calib(
+        calib_report_df: pd.DataFrame,
+        calibrated_steps: pd.DataFrame,
+        is_visible: bool = True,
+    ):
         regressors = calib_report_df["regressor"].unique().tolist()
         nrows = math.ceil(len(regressors) / 2)
 
@@ -205,14 +236,27 @@ class CalibrationProcess:
             y_priors_lower = y_priors - calib_report_df.loc[mask, "sigma_prior"].values
 
             axes[idx].set_title(ch)
-            axes[idx].plot(x, y_posteriors, label="posteriors", color="orange", alpha=0.5)
-            axes[idx].plot(x,y_priors, linestyle="--", label="priors", color="dodgerblue", alpha=0.5)
-            axes[idx].fill_between(x, y_priors_lower, y_priors_upper, alpha=0.2, color="dodgerblue")
+            axes[idx].plot(
+                x, y_posteriors, label="posteriors", color="orange", alpha=0.5
+            )
+            axes[idx].plot(
+                x,
+                y_priors,
+                linestyle="--",
+                label="priors",
+                color="dodgerblue",
+                alpha=0.5,
+            )
+            axes[idx].fill_between(
+                x, y_priors_lower, y_priors_upper, alpha=0.2, color="dodgerblue"
+            )
 
-            mask = calibrated_steps['regressor'] == ch
+            mask = calibrated_steps["regressor"] == ch
             if np.any(mask):
-                x_steps = calibrated_steps.loc[mask, 'iter']                
-                axes[idx].scatter(x_steps, y_posteriors[x_steps], color="orange", label="calibration")
+                x_steps = calibrated_steps.loc[mask, "iter"]
+                axes[idx].scatter(
+                    x_steps, y_posteriors[x_steps], color="orange", label="calibration"
+                )
 
         fig.suptitle(
             "Coefficient Calibration", y=0.92, fontsize=24, verticalalignment="center"
@@ -240,7 +284,7 @@ class CalibrationProcess:
     def select_calib_channels(
         self,
         report_df: pd.DataFrame,
-        se_tolerance: float = 1.,
+        se_tolerance: float = 1.0,
     ) -> List[str]:
         """Given a attribution report dataframe, recommend and return
         the list of channels should be calibrated
@@ -253,23 +297,19 @@ class CalibrationProcess:
             List[str]: recommended channels
         """
         # filter zero spend and within tolerance channels
-        select_mask = (
-            (report_df['spend'] > 1e-5) &
-            (np.abs(report_df['mmm_icac'] - report_df['test_icac']) > self.se_tolerance * report_df['test_se'])
-        ) 
+        select_mask = (report_df["spend"] > 1e-5) & (
+            np.abs(report_df["mmm_icac"] - report_df["test_icac"])
+            > self.se_tolerance * report_df["test_se"]
+        )
         reduced_ch_df = (
-            report_df
-            .loc[select_mask, :]
-            .sort_values(by="target_dist", ascending=False) 
+            report_df.loc[select_mask, :]
+            .sort_values(by="target_dist", ascending=False)
             .reset_index()
         )
         return reduced_ch_df["regressor"].tolist()
 
     @staticmethod
-    def append_model_info(
-        model: MMM, 
-        report_df: pd.DataFrame
-    ) -> pd.DataFrame:
+    def append_model_info(model: MMM, report_df: pd.DataFrame) -> pd.DataFrame:
         """Given a fitted model and the target data frame, attach regression
         related information to the end result data frame and return it.
 
@@ -291,7 +331,9 @@ class CalibrationProcess:
             }
         ).set_index("regressor")
 
-        expended_report = pd.concat([report_df, regression_summary], axis=1, join="inner")
+        expended_report = pd.concat(
+            [report_df, regression_summary], axis=1, join="inner"
+        )
         return expended_report
 
     @staticmethod
@@ -310,7 +352,7 @@ class CalibrationProcess:
         """
         # return new_ab_tests_df
         date_col = model.date_col
-        logger = logging.getLogger('karpiu-planning-test')
+        logger = logging.getLogger("karpiu-planning-test")
         logger.setLevel(30)
 
         report_df = ab_tests_df.copy()
@@ -318,14 +360,18 @@ class CalibrationProcess:
         report_df["mmm_attr"] = -1
         report_df["spend"] = -1
         # report_df = report_df.rename(columns={'test_channel': 'regressor'}).set_index('regressor')
-        
+
         for k in report_df.index:
             test_start = report_df.loc[k, "test_start"]
             test_end = report_df.loc[k, "test_end"]
-            attributor = AttributorGamma(model, start=test_start, end=test_end, logger=logger)
+            attributor = AttributorGamma(
+                model, start=test_start, end=test_end, logger=logger
+            )
             _, spend_attr, spend_df, _ = attributor.make_attribution()
 
-            mask = (spend_attr[date_col] >= test_start) & (spend_attr[date_col] <= test_end)
+            mask = (spend_attr[date_col] >= test_start) & (
+                spend_attr[date_col] <= test_end
+            )
             attr_sum = np.sum(spend_attr.loc[mask, k].values)
             mask = (spend_df[date_col] >= test_start) & (spend_df[date_col] <= test_end)
             cost_sum = np.sum(spend_df.loc[mask, k].values)
@@ -334,10 +380,14 @@ class CalibrationProcess:
             else:
                 report_df.loc[k, "mmm_icac"] = np.nan
             report_df.loc[k, "mmm_attr"] = np.round(attr_sum, 0)
-            report_df.loc[k, "spend"] =  np.round(cost_sum, 0)
+            report_df.loc[k, "spend"] = np.round(cost_sum, 0)
 
-        report_df["target_lift"] = np.round(report_df["spend"] / report_df["test_icac"], 0).astype(int)
-        report_df["target_dist"] = np.abs(report_df["target_lift"] - report_df["mmm_attr"])
+        report_df["target_lift"] = np.round(
+            report_df["spend"] / report_df["test_icac"], 0
+        ).astype(int)
+        report_df["target_dist"] = np.abs(
+            report_df["target_lift"] - report_df["mmm_attr"]
+        )
 
         return report_df
 
@@ -355,15 +405,23 @@ class CalibrationProcess:
         new_priors_df = model.get_regression_summary()[
             ["regressor", "loc_prior", "scale_prior"]
         ]
-        new_priors_df = new_priors_df.set_index('regressor')
+        new_priors_df = new_priors_df.set_index("regressor")
         new_priors_df = new_priors_df.loc[model.get_spend_cols()]
         if modify_channel is not None:
-            new_priors_df.loc[modify_channel, 'loc_prior'] = loc_prior
+            new_priors_df.loc[modify_channel, "loc_prior"] = loc_prior
             if sigma_prior is not None:
-                new_priors_df.loc[modify_channel, 'scale_prior'] = sigma_prior
+                new_priors_df.loc[modify_channel, "scale_prior"] = sigma_prior
             else:
-                new_priors_df.loc[modify_channel, 'scale_prior'] = max(loc_prior * 0.01, 1e-5)
-        new_priors_df = new_priors_df.reset_index().rename(columns={'regressor': 'test_channel', 'loc_prior': 'coef_prior', 'scale_prior': 'sigma_prior'})
+                new_priors_df.loc[modify_channel, "scale_prior"] = max(
+                    loc_prior * 0.01, 1e-5
+                )
+        new_priors_df = new_priors_df.reset_index().rename(
+            columns={
+                "regressor": "test_channel",
+                "loc_prior": "coef_prior",
+                "scale_prior": "sigma_prior",
+            }
+        )
         return new_priors_df
 
     # regressor, coef_prior, sigma_prior are the reserved keywords in main model
@@ -371,19 +429,23 @@ class CalibrationProcess:
     def _solve_channel_prior(self, model, regressor, calib_report):
         # test_channel = 'facebook'
         # ab_tests_df is static so it is okay to assume to be global
-        test_start = calib_report.loc[regressor, 'test_start']
-        test_end = calib_report.loc[regressor, 'test_end']
-        target = calib_report.loc[regressor, 'target_lift']
-        
+        test_start = calib_report.loc[regressor, "test_start"]
+        test_end = calib_report.loc[regressor, "test_end"]
+        target = calib_report.loc[regressor, "target_lift"]
+
         # always suppress solver logger
-        logger = logging.getLogger('karpiu-attr-solver')
+        logger = logging.getLogger("karpiu-attr-solver")
         logger.setLevel(30)
 
         def attr_obj_func(x, target):
-            attributor = AttributorGamma(model, start=test_start, end=test_end, logger=logger)
+            attributor = AttributorGamma(
+                model, start=test_start, end=test_end, logger=logger
+            )
             attr_res = attributor.make_attribution(new_coef_name=regressor, new_coef=x)
             _, spend_attr, _, _ = attr_res
-            mask = (spend_attr[model.date_col] >= test_start) & (spend_attr[model.date_col] <= test_end)
+            mask = (spend_attr[model.date_col] >= test_start) & (
+                spend_attr[model.date_col] <= test_end
+            )
             res = np.sum(spend_attr.loc[mask, regressor].values)
             loss = np.fabs(res - target)
             return loss
@@ -402,10 +464,14 @@ class CalibrationProcess:
         return coef_prior
 
     def get_calib_report(self) -> pd.DataFrame:
-        return pd.concat([df.reset_index() for df in self.calib_reports], ignore_index=True)
+        return pd.concat(
+            [df.reset_index() for df in self.calib_reports], ignore_index=True
+        )
 
     def get_calibrated_steps(self) -> pd.DataFrame:
-        return pd.DataFrame({
-            'iter': np.arange(1, len(self.calibrated_channels) + 1),
-            'regressor': self.calibrated_channels,
-        })
+        return pd.DataFrame(
+            {
+                "iter": np.arange(1, len(self.calibrated_channels) + 1),
+                "regressor": self.calibrated_channels,
+            }
+        )
